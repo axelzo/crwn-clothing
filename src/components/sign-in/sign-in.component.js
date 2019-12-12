e16f25a0165b69d13f1ab4ce4c3c5c9df5945e8c
Axel Malagon <jmalagon@intersysconsulting.com>
2019-12-12 16:40:03 -0600

    implemented firebase utils inlcuded ability to store authenticated users into ours firestore database

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in-styles.scss'
 
@@ -16,10 +16,18 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' })
+        } catch(error) {
+            console.log(error);
+        }
+        
     }
 
     handleChange = event => {
@@ -33,7 +41,7 @@ class SignIn extends React.Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
-                <form onSumbit={this.handleSubmit}>
+                <form onSubmit={this.handleSubmit}>
                     <FormInput
                         handleChange={this.handleChange}
                         name='email'
