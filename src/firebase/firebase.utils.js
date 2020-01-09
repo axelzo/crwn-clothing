4a522b8c51d2cddf0b95d2d7082f7c0ae7976e1a
Axel Malagon <jmalagon@intersysconsulting.com>
2020-01-09 10:43:58 -0600

    updating stripe integration and updating our favicon and title

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 
-//CONFIG HERE
-const config = {
-  apiKey: "AIzaSyBtyq9hBXWf6aDUOacJpPRfWJLSqVbxz8A",
-  authDomain: "crwn-db-fcccb.firebaseapp.com",
-  databaseURL: "https://crwn-db-fcccb.firebaseio.com",
-  projectId: "crwn-db-fcccb",
-  storageBucket: "crwn-db-fcccb.appspot.com",
-  messagingSenderId: "511413514599",
-  appId: "1:511413514599:web:aa9eba0c5f1d7ad164ffc2"
-}
+
 
   export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
