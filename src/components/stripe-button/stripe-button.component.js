48e47e1525917d041ea842a2a5626129a355dbf8
Axel Malagon <jmalagon@intersysconsulting.com>
2020-01-09 12:59:53 -0600

    Deploying

--- src/components/stripe-button/stripe-button.component.js
+++ src/components/stripe-button/stripe-button.component.js
@@ -3,7 +3,8 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
- 
+    const publishableKey = 'pk_test_bI7ssmTkepNz45K8kNSK5DsJ00iMroCgWR';
+    
 
     const onToken = token => {
         console.log(token);
